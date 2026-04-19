54e2fe8e313dec2ddabde47087ca76ab8d2b395b
louise moreno <morenolouisecuysona@gmail.com>
2026-04-20 00:24:49 +0800

    Revert "Merge pull request #26 from batdimoiprint/master"

--- src/api/inquiries.ts
+++ src/api/inquiries.ts
@@ -37,10 +37,7 @@ export const updateInquiryStatus = async (id: string, status: string): Promise<a
   return response.data;
 };
 
-export const scheduleInquiryMeeting = async (
-  id: string,
-  data: { date: string; time: string; location: string; organizerId: string }
-): Promise<any> => {
+export const scheduleInquiryMeeting = async (id: string, data: { date: string, time: string, location: string, organizerId: string }): Promise<any> => {
   const response = await axiosInstance.post(`/inquiries/${id}/meeting`, data);
   return response.data;
 };
